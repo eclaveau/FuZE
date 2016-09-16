4bbf6a1486f19e431e7629aad3d81471102d22fd
eclaveau <eclaveau@uw.edu>
2016-09-16 08:45:09 -0700

    Part of animation in subfunction

--- MagneticFieldAnimation.m
+++ MagneticFieldAnimation.m
@@ -24,7 +24,6 @@ M1y = 0;
 m1m0 = 0; % Generate animation for m1m0
 Mag = 1; % Generate animation for complete magnetic field
 savevideo =0 ; % Save a video of the magnetic field lines
-OnlyContour = 0; % Only plot contour on a separate figure
 SingleFig =0; % Only plot and saves individual figures of the contour
 % =========================================================================
 % save video
@@ -37,186 +36,31 @@ end
 % Definition of the data
 % ======================================================================
 
-% Angle and axial position at which the probes are placed
-theta = [0,pi/4,pi/2,3*pi/4,pi,5/4*pi,3/2*pi,7/4*pi,0];
-zp0 = [0, 0, 0, 0, 0, 0, 0, 0 ,0]; % Position of the probes on the z axis
-z0 = [0 5 10 15 20 25 30 35 40 45];
-% ppp=figure('units','normalized','outerposition',[0 0 1 1]); %,'visible','off'
-ppp = main_animation();
-% Display Properties
-ymargin = 1.05;
-fontsize = 10;
-subplot(16,8,[1 8])
-htitle = title('time');
-set(htitle,'FontSize',fontsize);
-axis off
-
 % Time range of display
 vis_start = find(data{1}>(max(data{1})/5),1,'first'); % Where to current first reach 10% of max current
 vis_end = find(data{1}>(max(data{1})/5),1,'last'); % Where the current last reach 10% of max current
 
-% I_P plot
-hip = subplot(16,8,[9 13 17 21 25 29]);
-Ip = plot(time(vis_start:vis_end), data{1}(vis_start:vis_end),'LineWidth',2);
-hold on
-timecursor_ip = plot(NaN,NaN);
-xlabel('Time [s]')
-ylabel('I [A]')
-hold off
-pos = get(gca, 'Position');
-pos(1) = pos(1);
-pos(2) = ymargin*pos(2);
-set(gca, 'Position', pos);
-axis([-inf inf 0 inf])
+% Angle and axial position at which the probes are placed
+theta = [0,pi/4,pi/2,3*pi/4,pi,5/4*pi,3/2*pi,7/4*pi,0];
+zp0 = [0, 0, 0, 0, 0, 0, 0, 0 ,0]; % Position of the probes on the z axis
+z0 = [0 5 10 15 20 25 30 35 40 45];
 
-if OnlyContour ==1
-    ax = gca;
-    set(ax,'XTickLabel',[]);
-    xlabel('')
-    title('a)','FontSize',fontsize)
-end
-
-% Normalized m1 to visualize quiescent period, from p5 to p45
-subplot(16,8,[33 37 41 45 49 53])
-m1_mean = data{106}(vis_start:vis_end);
-m1_sig = data{107}(vis_start:vis_end);
-plot(time(vis_start:vis_end)*1e6,m1_mean,'k','LineWidth',2);
-hold on
-plot(time(vis_start:vis_end)*1e6, m1_mean+m1_sig,'r');
-plot(time(vis_start:vis_end)*1e6, 0.2*ones(length(time(vis_start:vis_end)),1),'k')
-timecursor_m1m0 = plot(NaN,NaN);
-xlabel('Time [\mus]')
-ylabel('B_1/B_0')
-pos = get(gca, 'Position');
-pos(1) = pos(1);
-pos(2) = ymargin*pos(2);
-set(gca, 'Position', pos)
-axis([-inf inf -inf 0.5])
-ax = gca;
-set(ax,'YTick',[0,0.2,0.4]);
-hold off
-
-if OnlyContour ==1
-    ax = gca;
-    title('b)','FontSize',fontsize)
-    pos(2) = .95*pos(2);
-    set(gca, 'Position', pos)
-end
-
-if Mag == 1
-    % 3d plot of magnetic probe data
-    subplot(16,8,[14 16 22 24 30 32])% Top right bottom:[38 40 46 48 54 56]
-    h = plot3(NaN,NaN,NaN);
-    hold on
-    h5 = plot3(NaN,NaN,NaN);
-    h10 = plot3(NaN,NaN,NaN);
-    h15 = plot3(NaN,NaN,NaN);
-    h20 = plot3(NaN,NaN,NaN);
-    h25 = plot3(NaN,NaN,NaN);
-    h30 = plot3(NaN,NaN,NaN);
-    h35 = plot3(NaN,NaN,NaN);
-    h40 = plot3(NaN,NaN,NaN);
-    h45 = plot3(NaN,NaN,NaN);
-    zz00 = plot3(NaN,NaN,NaN);
-    axis([-max(data{56}) max(data{56}) 0 45 -max(data{56}) max(data{56})]);
-    xlabel('B [T]')
-    ylabel('z [cm]')
-    zlabel('B [T]')
-    daspect([1 20 1])
-    ax = gca;
-    set(ax,'YTick',[0,5,10,15,20,25,30,35,40,45]);
-    az = 130; el =5; % Point of view of the frame
-    view(az, el);
-    pos = get(gca, 'Position');
-    pos(1) = 1.*pos(1);
-    pos(2) = 1.08*pos(2);
-    set(gca, 'Position', pos)
-    grid on
-    
-    if OnlyContour ==1
-        set(ax,'YTickLabel',[]);
-        set(ax,'ZTickLabel',{-.2 0 .2});
-        set(ax,'XTickLabel',[]);
-        title('c)','FontSize',fontsize)
-        pos(2) =.98*pos(2);
-        set(gca, 'Position', pos)
-    end
-    hold off
-end
-
-if m1m0 == 1
-    % 3d plot of m1 and m0
-    subplot(16,8,[38 40 46 48 54 56])% Top right top:[14 16 22 24 30 32]
-    mm = plot3(NaN,NaN,NaN);
-    hold on
-    mm5 = plot3(NaN,NaN,NaN);
-    mm10 = plot3(NaN,NaN,NaN);
-    mm15 = plot3(NaN,NaN,NaN);
-    mm20 = plot3(NaN,NaN,NaN);
-    mm25 = plot3(NaN,NaN,NaN);
-    mm30 = plot3(NaN,NaN,NaN);
-    mm35 = plot3(NaN,NaN,NaN);
-    mm40 = plot3(NaN,NaN,NaN);
-    mm45 = plot3(NaN,NaN,NaN);
-    mm00 = plot3(NaN,NaN,NaN);
-    axis([-2.13/2*2.54 2.13/2*2.54 0 45 -2.13/2*2.54 2.13/2*2.54])
-    daspect([1 2 1])
-    xlabel('d [cm]')
-    ylabel('z [cm]')
-    zlabel('d [cm]')
-    % az = 50; el = 5; % 141202036
-    %     az = 130; el = 15; % 141201018
-    view(az, el);
-    ax = gca;
-    set(ax,'YTick',[0,5,10,15,20,25,30,35,40,45]);
-    set(ax,'ZTick',[-2.7,-2,-1,0,1,2,2.7]);
-    set(ax,'XTick',[-2,0,2]);
-    set(1,'color','w');
-    pos = get(gca, 'Position');
-    pos(1) = pos(1);
-    pos(2) = 1.07*pos(2);
-    set(gca, 'Position', pos)
-    grid on
-    hold off
-    if OnlyContour ==1
-        set(ax,'YTickLabel',[]);
-        set(ax,'ZTickLabel',{'',-2,'',0,'',2,''});
-        set(ax,'XTickLabel',[]);
-        %         xlabel('')
-        %         ylabel('')
-        %     zlabel('')
-        pos(2) = .93*pos(2);
-        set(gca, 'Position', pos)
-        title('d)','FontSize',fontsize)
-    end
-end
-
-if OnlyContour ==1
-    onlyc= figure('units','normalized','outerposition',[0 0 1 1]);
-    set(2,'color','w');
-end
-
-ax=subplot(16,8,[57 128]);
-[cont, hc] = contourf([], [], []);
-axis([-4 49 -40 360])
-xlabel('z [cm]')
-ylabel(['\theta [' 176 ']'])
-caxis([-1 1])
-range = -1:.1:1;
-ax.YTick = [0 45 90 135 180 225 270 315];
-colorbar
-colormap('jet')
-grid on
-fig=gcf;
-set(findall(fig,'-property','FontSize'),'FontSize',fontsize)
+% Figure initialization parameters
+[ppp, range, timecursor_ip, timecursor_m1m0, fontsize, htitle, h, h5, h10,...
+    h15, h20, h25, h30, h35, h40, h45, zz00, mm, mm5, mm10, mm15, mm20, ...
+    mm25, mm30, mm35, mm40, mm45, mm00, hc] = main_animation(vis_start, vis_end, time, data);
 
 k = 0;
+% Animation parameters. Start and end time.
 [t_pos,t_pos_start,t_pos_end] = startendtime(time,tstart,tend,timesteps);
 tic
 
 for t = t_pos
     k = k+1;
+    
+    % Progress tracking
     fprintf('%2.2f %%\n',(time(t)-time(t_pos_start))/(time(t_pos_end)-time(t_pos_start))*100)
+    
     % 8 array positions
     i = 2; % First position of the data in "node_string"
     b_p0 = [data{i}(t),data{i+1}(t),data{i+2}(t),data{i+3}(t),data{i+4}(t),data{i+5}(t),data{i+6}(t),data{i+7}(t),data{i}(t)]; % Value of the magnetic field at each angle with the last one repeating to have a complete circle
@@ -382,12 +226,6 @@ for t = t_pos
         set(mm45, 'XData', real(cx(:,10)), 'YData', zm(:,10), 'ZData', real(cy(:,10)), 'Linewidth', 1.5, 'Color', color(10), 'LineStyle', '-')
         set(mm00, 'XData', zeros(1,10), 'YData', z0, 'ZData', zeros(1,10), 'Linewidth', 2, 'Color', 'k', 'LineStyle', '-.')
         
-        %         % GIF Generation
-        %         frame=getframe(3);
-        %         im=frame2im(frame);
-        %         [imind,cmap]=rgb2ind(im,256);
-        %         imwrite(imind,cmap,['gifs\',num2str(shotnum), '_m1m0.gif'],'gif','WriteMode','append','DelayTime',delay);
-        %
     end
     % ======================================================================
     % Plot - Contour raw magnetic field
@@ -407,16 +245,6 @@ for t = t_pos
         writeVideo(video,image_pert)
     end
     
-    if SingleFig==1
-        tlt = sprintf('%s_%2.0f_%4.0f_microsec.tif',num2str(shotnum),floor(time(t)*1e6),10000*(time(t)*1e6-floor(time(t)*1e6)));
-        tlt_info = sprintf('Info_%s_%2.0f_%4.0f_microsec.tif',num2str(shotnum),floor(time(t)*1e6),10000*(time(t)*1e6-floor(time(t)*1e6)));
-        if OnlyContour ==1
-            imwrite(frame2im(getframe(onlyc)),tlt)
-            imwrite(frame2im(getframe(ppp)),tlt_info)
-        else
-            imwrite(frame2im(getframe(ppp)),tlt)
-        end
-    end
     % =====================================================================
     % Fourier spectrum in the axial direction at each time
     % =====================================================================
@@ -456,7 +284,6 @@ end
 % =========================================================================
 % Dynamic Mode Decomposition Part (DMD)
 % =========================================================================
-clear u_modes u_dmd
 % dt = time(2)-time(1);
 
 rank = 8;
@@ -464,33 +291,33 @@ b_untrans = b_th180;
 b_th180_T = b_th180';
 
 % Three modes svd reconstruction
-[Ut, Sigmat, Vt] = svd(b_th180_T, 'econ');
-figure(7)
-subplot(1,2,1)
-surf(Ut(:,1:3)*Sigmat(1:3,1:3)*Vt(:,1:3)')
-title('USV reconstruction with 3 first modes')
-subplot(1,2,2)
-surf(Ut(:,1:3)*Sigmat(1:3,1:3)*Vt(:,1:3)'-b_th180_T)
-title('Error between data and 3 modes reconstructions')
-err_svd = mean(mean(abs(Ut(:,1:3)*Sigmat(1:3,1:3)*Vt(:,1:3)'-b_th180_T)));
-
-figure(8)
-plot(diag(Sigmat)/sum(diag(Sigmat))*100,'.','LineWidth',3)
-title('Principal Components [%]')
-
-figure(9)
-ii = 0;
-for kk = [1 2 5 6]
-    ii = ii +1;
-    subplot(4,2,kk)
-    plot(Ut(:,ii))
-    ident = [num2str(ii) ' Spatial Mode'];
-    title(ident)
-    subplot(4,2,kk+2)
-    plot(10^6*time(t_pos_start:t_pos_end),abs(Vt(:,ii)))
-    ident = [num2str(ii) ' Temporal Mode'];
-    title(ident)
-end
+% [Ut, Sigmat, Vt] = svd(b_th180_T, 'econ');
+% figure(7)
+% subplot(1,2,1)
+% surf(Ut(:,1:3)*Sigmat(1:3,1:3)*Vt(:,1:3)')
+% title('USV reconstruction with 3 first modes')
+% subplot(1,2,2)
+% surf(Ut(:,1:3)*Sigmat(1:3,1:3)*Vt(:,1:3)'-b_th180_T)
+% title('Error between data and 3 modes reconstructions')
+% err_svd = mean(mean(abs(Ut(:,1:3)*Sigmat(1:3,1:3)*Vt(:,1:3)'-b_th180_T)));
+% 
+% figure(8)
+% plot(diag(Sigmat)/sum(diag(Sigmat))*100,'.','LineWidth',3)
+% title('Principal Components [%]')
+% 
+% figure(9)
+% ii = 0;
+% for kk = [1 2 5 6]
+%     ii = ii +1;
+%     subplot(4,2,kk)
+%     plot(Ut(:,ii))
+%     ident = [num2str(ii) ' Spatial Mode'];
+%     title(ident)
+%     subplot(4,2,kk+2)
+%     plot(10^6*time(t_pos_start:t_pos_end),abs(Vt(:,ii)))
+%     ident = [num2str(ii) ' Temporal Mode'];
+%     title(ident)
+% end
 
 % X1 = b_th180_T(:,1:end-1);
 % X2 = b_th180_T(:,2:end);
@@ -572,6 +399,7 @@ xlabel('t')
 ylabel('x')
 title('Error')
 
+%%
 % =========================================================================
 % Modes visualization
 % =========================================================================
--- main_animation.m
+++ main_animation.m
@@ -1,5 +1,129 @@
-function [ppp] = main_animation()
+function [ppp, range, timecursor_ip, timecursor_m1m0, fontsize, htitle, h, h5, h10, h15, h20, h25, h30, h35, h40, h45, zz00, mm, mm5, mm10, mm15, mm20, mm25, mm30, mm35, mm40, mm45, mm00, hc] = main_animation(vis_start, vis_end, time, data)
 
 ppp=figure('units','normalized','outerposition',[0 0 1 1]);
 
+% Display Properties
+ymargin = 1.05;
+fontsize = 10;
+
+% Title of whole figure
+subplot(16,8,[1 8])
+htitle = title('time');
+set(htitle,'FontSize',fontsize);
+axis off
+
+% I_P plot
+hip = subplot(16,8,[9 13 17 21 25 29]);
+Ip = plot(time(vis_start:vis_end), data{1}(vis_start:vis_end),'LineWidth',2);
+hold on
+timecursor_ip = plot(NaN,NaN);
+xlabel('Time [s]')
+ylabel('I [A]')
+hold off
+pos = get(gca, 'Position');
+pos(1) = pos(1);
+pos(2) = ymargin*pos(2);
+set(gca, 'Position', pos);
+axis([-inf inf 0 inf])
+
+% Normalized m1 to visualize quiescent period, from p5 to p45
+
+m1_mean = data{106}(vis_start:vis_end);
+m1_sig = data{107}(vis_start:vis_end);
+
+subplot(16,8,[33 37 41 45 49 53])
+plot(time(vis_start:vis_end)*1e6,m1_mean,'k','LineWidth',2);
+hold on
+plot(time(vis_start:vis_end)*1e6, m1_mean+m1_sig,'r');
+plot(time(vis_start:vis_end)*1e6, 0.2*ones(length(time(vis_start:vis_end)),1),'k')
+timecursor_m1m0 = plot(NaN,NaN);
+xlabel('Time [\mus]')
+ylabel('B_1/B_0')
+pos = get(gca, 'Position');
+pos(1) = pos(1);
+pos(2) = ymargin*pos(2);
+set(gca, 'Position', pos)
+axis([-inf inf -inf 0.5])
+ax = gca;
+set(ax,'YTick',[0,0.2,0.4]);
+hold off
+
+% 3d plot of magnetic probe data
+subplot(16,8,[14 16 22 24 30 32])% Top right bottom:[38 40 46 48 54 56]
+h = plot3(NaN,NaN,NaN);
+hold on
+h5 = plot3(NaN,NaN,NaN);
+h10 = plot3(NaN,NaN,NaN);
+h15 = plot3(NaN,NaN,NaN);
+h20 = plot3(NaN,NaN,NaN);
+h25 = plot3(NaN,NaN,NaN);
+h30 = plot3(NaN,NaN,NaN);
+h35 = plot3(NaN,NaN,NaN);
+h40 = plot3(NaN,NaN,NaN);
+h45 = plot3(NaN,NaN,NaN);
+zz00 = plot3(NaN,NaN,NaN);
+axis([-max(data{56}) max(data{56}) 0 45 -max(data{56}) max(data{56})]);
+xlabel('B [T]')
+ylabel('z [cm]')
+zlabel('B [T]')
+daspect([1 20 1])
+ax = gca;
+set(ax,'YTick',[0,5,10,15,20,25,30,35,40,45]);
+az = 130; el =5; % Point of view of the frame
+view(az, el);
+pos = get(gca, 'Position');
+pos(1) = 1.*pos(1);
+pos(2) = 1.08*pos(2);
+set(gca, 'Position', pos)
+grid on
+hold off
+
+% 3d plot of m1 and m0
+subplot(16,8,[38 40 46 48 54 56])% Top right top:[14 16 22 24 30 32]
+mm = plot3(NaN,NaN,NaN);
+hold on
+mm5 = plot3(NaN,NaN,NaN);
+mm10 = plot3(NaN,NaN,NaN);
+mm15 = plot3(NaN,NaN,NaN);
+mm20 = plot3(NaN,NaN,NaN);
+mm25 = plot3(NaN,NaN,NaN);
+mm30 = plot3(NaN,NaN,NaN);
+mm35 = plot3(NaN,NaN,NaN);
+mm40 = plot3(NaN,NaN,NaN);
+mm45 = plot3(NaN,NaN,NaN);
+mm00 = plot3(NaN,NaN,NaN);
+axis([-2.13/2*2.54 2.13/2*2.54 0 45 -2.13/2*2.54 2.13/2*2.54])
+daspect([1 2 1])
+xlabel('d [cm]')
+ylabel('z [cm]')
+zlabel('d [cm]')
+% az = 50; el = 5; % 141202036
+% az = 130; el = 15; % 141201018
+view(az, el);
+ax = gca;
+set(ax,'YTick',[0,5,10,15,20,25,30,35,40,45]);
+set(ax,'ZTick',[-2.7,-2,-1,0,1,2,2.7]);
+set(ax,'XTick',[-2,0,2]);
+set(1,'color','w');
+pos = get(gca, 'Position');
+pos(1) = pos(1);
+pos(2) = 1.07*pos(2);
+set(gca, 'Position', pos)
+grid on
+hold off
+
+% Final figure parameters
+ax=subplot(16,8,[57 128]);
+[cont, hc] = contourf([], [], []);
+axis([-4 49 -40 360])
+xlabel('z [cm]')
+ylabel(['\theta [' 176 ']'])
+caxis([-1 1])
+range = -1:.1:1;
+ax.YTick = [0 45 90 135 180 225 270 315];
+colorbar
+colormap('jet')
+grid on
+fig=gcf;
+set(findall(fig,'-property','FontSize'),'FontSize',fontsize)
 end
