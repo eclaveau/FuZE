3240cc0fec4879748f8d282609e5835197a3fd91
eclaveau <eclaveau@uw.edu>
2016-09-20 23:55:26 -0700

    Need to add condition so that it doesn't crash at ii == 10

--- MagneticFieldAnimation_Manual.m
+++ MagneticFieldAnimation_Manual.m
@@ -5,6 +5,7 @@ clc; clear all;
 % Part of the program you want to run
 SaveLocal = 0; % Saves the workspace to a file (For use on a computer not connected to MDSplus)
 LoadLocal = 1; % Load from existing file
+
 % =========================================================================
 shotnum = 151027024;
 name = [num2str(shotnum) '_workspace'];
@@ -15,14 +16,15 @@ else
 end
 
 if SaveLocal == 1
+    SaveLocal = 0;
     save(name); % Save workspace to a local file
 end
 
-
+Animate_and_save = 0;
 % ======================================================================
 % Visualization parameters
 % ======================================================================
-
+if Animate_and_save == 1
 % Starting and ending time parameters  62.239 for the animation
 tstart = 61.239; tend = 0; timesteps = 110;
 
@@ -33,6 +35,10 @@ vis_end = find(data{1}>(max(data{1})/5),1,'last'); % Where the current last reac
 
 % Animation of magnetic field
 [m1, phi, m0, b_th180, t_pos_start, t_pos_end, z0] = main_animation(vis_start, vis_end, shotnum, time, data, tstart, tend, timesteps);
+save([name '_data_only'], 'm1', 'phi', 'm0', 'b_th180', 't_pos_start', 't_pos_end', 'z0')
+else
+    load([name '_data_only'])
+end
 window_time = time(t_pos_start:t_pos_end);
 %%
 % =========================================================================
@@ -101,42 +107,63 @@ L = 1; % Peak number at one array
 ii = 1; % Z location
 end_cond = 0; % Condition to end looking for peaks.
 while end_cond == 0;
+    if K_s == 1 % First point in the streak
+            L_s = L; % Initial peak that you started
+            ii_s = ii; % Initial array that you started from
+            streak{K} = [t_ind{ii}(L), ii];
+    end
+    
     if sum(t_ind{ii+1}>t_ind{ii}(L))>=1 % There is a future peak in the next axial array
         
         
         t_ind_next = t_ind{ii+1}(find(t_ind{ii+1}>t_ind{ii}(L),1)); % indice of time of the next peak at the next axial array
         
         % Parameter for the streak for 2 points. [times, pos]
-        if K_s ==1 % First point in the streak
-            streak{K} = [t_ind{ii}(L), ii];
-        end
         
         streak{K}(K_s + 1, 1) = t_ind_next;  
         streak{K}(K_s + 1, 2) = ii + 1;
         %     phid_loc = phid(locs{ii}(1),ii); % phase of the local peak
         %     phid_next = phid(loc_next,ii+1); % phase of the next location
+   
+        
+  
         %     if phid_loc-30<phid_next && phid_next<phid_loc+30 % Range of phase of the two peak
         %     end
         %     L = L + 1; % Look for the next peak in the same array.
-        
-        ii = ii + 1; % Look at the next array
+       
         L = find(t_ind{ii+1}>t_ind{ii}(L),1); % Position at which the next point is
+        ii = ii + 1; % Look at the next array
         K_s = K_s + 1; % Number of points in the streak.
-    else
-        ii = ii+1; % If no future peak, look from the next axial array.
-        K = K + 1
+    else % No future peaks
+        if L_s == length(t_ind{ii_s}) % If you are at the last peak of that array.
+            ii = ii_s+1; % look from the next axial array.
+            L = 1; % First point of the new array
+        else % Else look for the next peak.
+            L = L_s + 1; % Next peak
+            ii = ii_s; % Initial position where you started
+        end
+        K = K + 1; % New streak
+        K_s = 1; % number of points in the streak is restarted
     end
-    
-    if ii == 10 % Reached the end of the arrays.
+    ii_s
+    if ii_s == 10 % Reached the end of the arrays.
     end_cond = 1;
-    end
+    endii
 end
 % Streaks, line connecting consecutive peaks
-for kk = 1:size(streak{K},1)
-streak_time{K}(kk) = 1e6*window_time(streak{K}(kk,1));
-streak_pos{K}(kk) = 5*(streak{K}(kk,2)-1);
+nK = K; % Total number of streaks
+for K = 1:nK
+    if isempty(streak{K}) % Streak where no peaks were found.
+        streak_time{K} = [];
+        streak_pos{K} = [];
+    end
+    for kk = 1:size(streak{K},1)
+        streak_time{K}(kk) = 1e6*window_time(streak{K}(kk,1));
+        streak_pos{K}(kk) = 5*(streak{K}(kk,2)-1);
+    end
+    plot(streak_time{K}, streak_pos{K})
 end
-plot(streak_time{K}, streak_pos{K})
+
 xlabel('time [us]')
 ylabel('position [cm]')
 
